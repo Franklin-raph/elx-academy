9be8f4f02138d7760cbedd2ea733e1c633908b23
franklin-raph <igboekwulusifranklin@gmail.com>
2023-02-01 17:23:38 +0100

    bug fix

--- elx-server/config/connectDb.js
+++ elx-server/config/connectDb.js
@@ -1,17 +1,18 @@
-const mongoose = require('mongoose')
-require('dotenv').config();
-const dbConnectionString = process.env.MONGO_LOCAL_URI
-mongoose.set('strictQuery', true);
+const mongoose = require("mongoose");
+require("dotenv").config();
+const dbConnectionString = process.env.MONGO_REMOTE_URI;
+console.log(dbConnectionString);
+mongoose.set("strictQuery", true);
 
 const dbConnectinMethod = async () => {
-    console.log(dbConnectionString)
-    try {
-        await mongoose.connect(dbConnectionString)
-        console.log("Db connected")
-    } catch (error) {
-        console.log(error)
-        process.exit(1)
-    }
-}
+  console.log(dbConnectionString);
+  try {
+    await mongoose.connect(dbConnectionString);
+    console.log("Db connected");
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
 
-module.exports = { dbConnectinMethod }
+module.exports = { dbConnectinMethod };
